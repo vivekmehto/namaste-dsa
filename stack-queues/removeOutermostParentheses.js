186125ef119f7335674a2baf4ea50a7e052425c2
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-11-17 12:32:15 +0530

    Remove Outermost Parentheses without stack

--- stack-queues/removeOutermostParentheses.js
+++ stack-queues/removeOutermostParentheses.js
@@ -71,19 +71,35 @@
 // Space Complexity = O(n)
 
 var removeOuterParentheses = function (s) {
-  let stack = [];
+  // let stack = [];
+  // let ans = "";
+
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s[i] === "(") {
+  //     stack.push(s[i]);
+  //     let len = stack.length;
+  //     if (len > 1) {
+  //       ans = ans + s[i];
+  //     }
+  //   } else {
+  //     ans += stack.length > 1 ? s[i] : "";
+  //     stack.pop();
+  //   }
+  // }
+  // return ans;
+
+  // without stack
+
+  let level = 0;
   let ans = "";
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] === "(") {
-      stack.push(s[i]);
-      let len = stack.length;
-      if (len > 1) {
-        ans = ans + s[i];
-      }
+      level++;
+      ans += level > 1 ? s[i] : "";
     } else {
-      ans += stack.length > 1 ? s[i] : "";
-      stack.pop();
+      ans += level > 1 ? s[i] : "";
+      level--;
     }
   }
   return ans;
