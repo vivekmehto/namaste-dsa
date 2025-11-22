b27d9ff255524748e4a1ccd102f283637cb726ba
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-11-22 21:47:45 +0530

    Next Greater Element II code

--- stack-queues/nextGreaterElement2.js
+++ stack-queues/nextGreaterElement2.js
@@ -54,3 +54,23 @@
 
 // Space Complexity:
 // Space Complexity = O(n)
+
+var nextGreaterElements = function (arr) {
+  let n = arr.length;
+  let stack = [];
+  let ans = Array(n).fill(-1);
+  stack.push(arr[n - 1]);
+  for (let i = 2 * n - 2; i >= 0; i--) {
+    while (stack.length) {
+      let top = stack[stack.length - 1];
+      if (arr[i % n] < top) {
+        ans[i % n] = top;
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+    stack.push(arr[i % n]);
+  }
+  return ans.slice(0, n);
+};
