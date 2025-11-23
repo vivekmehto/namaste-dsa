01001bffce13b5d7304f5b7cdd9804e18c2aa636
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-11-23 21:05:44 +0530

    Rotting Oranges

---
+++ stack-queues/rottingOranges.js
@@ -0,0 +1,52 @@
+// Rotting Oranges
+// Problem Statement:
+// You are given an m x n grid where each cell can have one of three values:
+
+// 0 representing an empty cell,
+// 1 representing a fresh orange, or
+// 2 representing a rotten orange.
+// Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+// Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+// Examples:
+// Example 1:
+
+// Input:
+
+// grid = [[2,1,1],[0,1,1],[1,0,1]]
+
+// Output: -1
+
+// Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+// Example 2:
+
+// Input:
+
+// grid = [[0,2]]
+
+// Output: 0
+
+// Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+// For more details, refer to LeetCode problem 994.
+
+// Constraints:
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 10
+// grid[i][j] is 0, 1, or 2.
+// Approach:
+// Identify rotten oranges (2) and push their positions + time ([i, j, 0]) into a queue.
+// Use BFS to spread rot in 4 directions (up, down, left, right).
+// For each direction: If neighbor is a fresh orange (1), rot it (→ 2) and push it into the queue with time +1.
+// Track time with maxMinutes.
+// After BFS, if any fresh orange (1) left, return -1.
+// Else, return maxMinutes.
+
+// Time Complexity:
+// Time Complexity = O(m x n)
+
+// Space Complexity:
+// Space Complexity = O(m x n)
