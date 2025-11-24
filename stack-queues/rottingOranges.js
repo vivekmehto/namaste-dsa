56560da67650bdb9ec1d0e4a9c6de7f1664a97f2
Vivek Mehto <vivekmehto.webdev@gmail.com>
2025-11-24 12:46:56 +0530

    Rotting Oranges code

--- stack-queues/rottingOranges.js
+++ stack-queues/rottingOranges.js
@@ -50,3 +50,46 @@
 
 // Space Complexity:
 // Space Complexity = O(m x n)
+
+var orangesRotting = function (grid) {
+  let m = grid.length;
+  let n = grid[0].length;
+  let queue = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      }
+    }
+  }
+  let maxMinutes = 0;
+  while (queue.length) {
+    let [x, y, level] = queue.shift();
+
+    if (x > 0 && grid[x - 1][y] === 1) {
+      grid[x - 1][y] = 2;
+      queue.push([x - 1, y, level + 1]);
+    }
+    if (x < m - 1 && grid[x + 1][y] === 1) {
+      grid[x + 1][y] = 2;
+      queue.push([x + 1, y, level + 1]);
+    }
+    if (y < n - 1 && grid[x][y + 1] === 1) {
+      grid[x][y + 1] = 2;
+      queue.push([x, y + 1, level + 1]);
+    }
+    if (y > 0 && grid[x][y - 1] === 1) {
+      grid[x][y - 1] = 2;
+      queue.push([x, y - 1, level + 1]);
+    }
+    maxMinutes = Math.max(level, maxMinutes);
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 1) {
+        return -1;
+      }
+    }
+  }
+  return maxMinutes;
+};
