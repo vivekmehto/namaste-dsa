f116df5c17058e261865c9939a358bd96a8e3ed9
vivekmehto <vivekmehto.webdev@gmail.com>
2025-12-10 11:55:49 +0530

    ADD: Guess Higher or Lower Number

---
+++ binarySearchAlgorithm/guesHigherOrLower.js
@@ -0,0 +1,17 @@
+const guessHigherOrLower = (n) => {
+  let low = 1;
+  let high = n;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let res = guess(mid);
+
+    if (res === 0) {
+      return mid; 
+    } else if (res === -1) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }  
+    }
+}
